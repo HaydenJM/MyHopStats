125d3aa6ce9281f78dd77b0a7e171a37ab281600
Hayden M. <40805533+HaydenJM@users.noreply.github.com>
2025-05-27 00:08:46 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server started on port ${PORT}`);
